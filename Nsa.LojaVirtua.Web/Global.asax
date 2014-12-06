149c2f532d741dc999df92e16a803d094b04acfa
netodias_ <abdneto@hotmail.com>
2014-12-05 23:04:31 -0300

    Initial Commit

---
+++ Nsa.LojaVirtua.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nsa.LojaVirtua.Web.MvcApplication" Language="C#" %>
---
+++ Nsa.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nsa.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestNsa
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
